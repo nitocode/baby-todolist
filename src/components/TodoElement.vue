b04b8652b3abc0046a0c038cbb0c317cd7547241
nitocode <lu.nitoumbi@gmail.com>
2022-10-09 01:10:59 +0200

    WIP: todo list without element deletion

---
+++ src/components/TodoElement.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div
+      class="element-group"
+      :class="{ 'element-group--checked': props.element.checked }"
+    >
+      <div class="handle">O</div>
+      <input type="checkbox" v-model="props.element.checked" />
+      <input class="element-text" type="text" v-model="props.element.name" />
+      <button @click="deleteElement()">X</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps(["element"]);
+
+const emit = defineEmits<{
+  (e: "delete", id: number): void;
+}>();
+
+const deleteElement = (): void => {
+  emit("delete", props.element.id);
+};
+</script>
+
+<style lang="scss" scoped>
+.element-group {
+  display: flex;
+  &--checked {
+    .element-text {
+      text-decoration: line-through;
+    }
+  }
+  input[type="text"],
+  input[type="text"]:focus,
+  input[type="text"]:focus-visible,
+  input[type="text"]:active {
+    border: none;
+    outline: none;
+    background: transparent;
+  }
+  input[type="checkbox"] {
+    margin-right: 10px;
+    cursor: pointer;
+  }
+  .handle {
+    cursor: move;
+    margin-right: 10px;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,124 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import draggable from "vuedraggable";
+import TodoElement from "./../components/TodoElement.vue";
+// import nestedDraggable from "./components/Nested.vue";
+const drag = ref<boolean>(false);
+// const daddyList = ref<Array<{ name: string; id: number; order: number }>>([]);
+const daddyList = ref<any>([]);
+daddyList.value = [
+  {
+    name: "Rentrer",
+    id: 1,
+    order: 1,
+    checked: false,
+    tasks: [
+      {
+        name: "Livret de famille",
+        id: 2,
+        order: 1,
+        checked: false,
+      },
+      {
+        name: "Documents échographies",
+        id: 3,
+        order: 2,
+        checked: false,
+      },
+      {
+        name: "Carte d'identité Maman",
+        id: 4,
+        order: 3,
+        checked: false,
+      },
+    ],
+  },
+  {
+    name: "Soutenir",
+    id: 5,
+    order: 2,
+    checked: false,
+    tasks: [
+      {
+        name: "Stylo",
+        id: 6,
+        order: 1,
+        checked: false,
+      },
+      {
+        name: "Sac à main Maman",
+        id: 7,
+        order: 2,
+        checked: true,
+      },
+      {
+        name: "Portable Maman",
+        id: 8,
+        order: 3,
+        checked: false,
+      },
+    ],
+  },
+];
+
+const dragOptions = computed(() => {
+  return {
+    animation: 200,
+    group: "description",
+    disabled: false,
+    ghostClass: "ghost",
+  };
+});
+
+const deleteElement = (id: number) => {
+  console.log("id", id);
+};
+</script>
 
 <template>
   <main>
     <div>
       <p>HOME PAGE !!!!</p>
+
+      <draggable
+        class="list-group"
+        tag="ul"
+        v-model="daddyList"
+        v-bind="dragOptions"
+        handle=".handle"
+        @start="drag = true"
+        @end="drag = false"
+      >
+        <template #item="{ element }">
+          <div>
+            <todo-element @delete="deleteElement" :element="element" />
+
+            <draggable
+              class="list-group"
+              tag="ul"
+              v-model="element.tasks"
+              v-bind="dragOptions"
+              handle=".handle"
+              @start="drag = true"
+              @end="drag = false"
+            >
+              <template #item="{ element }">
+                <todo-element @delete="deleteElement" :element="element" />
+              </template>
+            </draggable>
+          </div>
+        </template>
+      </draggable>
     </div>
   </main>
 </template>
+
+<style lang="scss" scoped>
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+.list-group {
+  min-height: 20px;
+}
+</style>
